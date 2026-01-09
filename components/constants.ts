ec06ba91b8e03c0891e57c14f5f0afecdd276d69
Cesar Garces <cesargarce@gmail.com>
2026-01-09 18:22:39 -0500

    Refactor Contact component by removing the form functionality and enhancing the layout for social links, improving user experience and code clarity.

--- components/constants.ts
+++ components/constants.ts
@@ -1,6 +1,6 @@
 export const SOCIAL_LINKS = {
+  whatsapp: 'https://wa.me/573507424296',
   email: 'mailto:info@cesargarces.com',
   linkedin: 'https://www.linkedin.com/in/cesargarces/',
   github: 'https://github.com/CesarGarces',
-  whatsapp: 'https://wa.me/573507424296',
 } as const
